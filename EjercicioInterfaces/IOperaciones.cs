8c7075b05af2fd81642e507dcf228ac1ec8c90db
Gerardo Perez <113952421+g3rar2@users.noreply.github.com>
2025-02-09 14:25:27 -0600

    add Interface

---
+++ EjercicioInterfaces/IOperaciones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioInterfaces
+{
+    public interface IOperaciones
+    {
+
+        public double Area();
+        public double Hipotenusa();
+
+        public double Volumen();
+
+        public double Dolares();
+
+        public double Farenheith();
+
+
+
+    }
+}
