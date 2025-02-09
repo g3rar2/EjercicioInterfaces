6f9ef89108cba9f6bc4574227d09dd14de2947a7
Gerardo Perez <113952421+g3rar2@users.noreply.github.com>
2025-02-09 14:48:24 -0600

    implementation

--- EjercicioInterfaces/Program.cs
+++ EjercicioInterfaces/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using EjercicioInterfaces;
+
+Operaciones operaciones = new Operaciones();
+
+Console.Write("Ingrese el primer lado del Triangulo: ");
+operaciones.lado1 =int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el segundo lado del Triangulo: ");
+operaciones.lado2 = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese la altura del Prisma: ");
+operaciones.altura = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese la cantidad de lempiras a convertir: ");
+operaciones.lempiras = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese los grados celsius a convertir: ");
+operaciones.celsius = int.Parse(Console.ReadLine());
+
+
+operaciones.Imprimir();
